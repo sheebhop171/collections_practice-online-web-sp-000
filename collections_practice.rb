40e4b27b3ae6fe76e447c52c72cbc0c30975b145
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-29 19:49:31 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  #array.sort! {|x,y| y <=> x}
+  array.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array [2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.select {|word| word[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|word| word.chars.first == "a" }
+end
+
+def sum_array(array)
+  array.sum
+end
+
+def add_s(array)
+  array.collect do |element|
+    if element == array[1]
+        element
+    else
+        element + 's'
+    end
+  end
+end
